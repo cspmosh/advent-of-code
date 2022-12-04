e8390ae58895ced9e46895b059c9bfa124f2a3cf
JTurner <cs.pmosh@gmail.com>
2022-12-04 14:35:32 -0600

    checks if any overlaps exist in ranged shifts

---
+++ spec/day_04/shift_spec.rb
@@ -0,0 +1,36 @@
+require 'day_04/shift'
+
+describe Shift do
+  describe "#covered_by?" do
+    let(:first_shift) { Shift.new("4-8") }
+
+    it "returns true if the shift is fully covered by the other" do
+      expect(Shift.new("5-7").covered_by?(first_shift)).to be true
+      expect(Shift.new("8-8").covered_by?(first_shift)).to be true
+    end
+
+    it "returns false if the shift is not fully covered by the other" do
+      expect(Shift.new("1-4").covered_by?(first_shift)).to be false
+      expect(Shift.new("7-10").covered_by?(first_shift)).to be false
+      expect(Shift.new("20-30").covered_by?(first_shift)).to be false
+      expect(Shift.new("1-10").covered_by?(first_shift)).to be false
+    end
+  end
+
+  describe "#overlaps_with?" do
+    let(:first_shift) { Shift.new("4-8") }
+
+    it "returns true if the schedule overlaps with the provided shift" do
+      expect(Shift.new("5-7").overlaps_with?(first_shift)).to be true
+      expect(Shift.new("8-8").overlaps_with?(first_shift)).to be true
+      expect(Shift.new("1-4").overlaps_with?(first_shift)).to be true
+      expect(Shift.new("7-10").overlaps_with?(first_shift)).to be true
+      expect(Shift.new("1-10").overlaps_with?(first_shift)).to be true
+    end
+
+    it "returns false if the schedule does not overlap with the provided shift" do
+      expect(Shift.new("20-30").overlaps_with?(first_shift)).to be false
+      expect(Shift.new("1-3").overlaps_with?(first_shift)).to be false
+    end
+  end
+end
