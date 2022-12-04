d7ea0df34e698a86b253790b8c91d83e15580706
Josh "Bootstrap" Turner <cs.pmosh@gmail.com>
2022-12-04 14:35:52 -0600

    checks if any overlaps exist in ranged shifts (#1)

---
+++ lib/day_04/main.rb
@@ -0,0 +1,16 @@
+require_relative 'shift'
+
+fully_contained_shifts = 0
+overlapping_shifts = 0
+
+File.foreach("data/d4_section_assignments.txt", chomp: true) do |schedules|
+  shift_range_1, shift_range_2 = schedules.split(",")
+  shift_1 = Shift.new(shift_range_1)
+  shift_2 = Shift.new(shift_range_2)
+
+  fully_contained_shifts += 1 if shift_1.covered_by?(shift_2) || shift_2.covered_by?(shift_1)
+  overlapping_shifts += 1 if shift_1.overlaps_with?(shift_2)
+end
+
+puts "fully contained shifts: " + fully_contained_shifts.to_s
+puts "overlapping shifts: " + overlapping_shifts.to_s
---
+++ lib/day_04/shift.rb
@@ -0,0 +1,20 @@
+class Shift
+  attr_reader :first_day, :last_day, :range
+
+  def initialize(range_text)
+    @first_day, @last_day = range_text.split("-").map(&:to_i)
+  end
+
+  def range
+    (first_day..last_day)
+  end
+
+  def covered_by?(other_shift)
+    other_shift.range.cover?(first_day) && other_shift.range.cover?(last_day) #503
+  end
+
+  def overlaps_with?(other_shift)
+    other_shift.range.cover?(first_day) || other_shift.range.cover?(last_day) ||
+    range.cover?(other_shift.first_day) || range.cover?(other_shift.last_day)
+  end
+end
